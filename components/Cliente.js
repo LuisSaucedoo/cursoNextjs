3b822ed87fbd09d0ab7a893d46d3461e12dc04e6
Luis Saucedo <luigisaucedo.ls@gmail.com>
2022-06-16 18:05:26 -0400

    Next.js : Formulario para editar un cliente

--- components/Cliente.js
+++ components/Cliente.js
@@ -120,7 +120,7 @@ const Cliente = ({cliente}) => {
 
                     {/* heroicons.com - Para buscar la foto del botón en modo Copy JSX */}
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                     </svg>
 
                 </button>
