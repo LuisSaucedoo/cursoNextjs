55db55c6022441b9d062fcf5953b9080afeab323
Luis Saucedo <luigisaucedo.ls@gmail.com>
2022-06-19 19:15:03 -0400

    Sección Producto: Routing para edición de productos

---
+++ pages/editarproducto/[pid].js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import { useRouter } from 'next/router';
+
+const EditarProducto = () => {
+    const router = useRouter();
+    const { query: {pid: id } } = router;
+    // console.log(id);
+
+
+    return ( 
+        <Layout>
+            <h1 className="text-2xl text-gray-800 font-light">Editar Producto</h1>
+
+
+
+        </Layout>
+     );
+}
+ 
+export default EditarProducto;
