90a820492327900e7df0bb11417f57d5676acb0d
Luis Saucedo <luigisaucedo.ls@gmail.com>
2022-06-20 18:54:49 -0400

    Sección Final: Graficando a los Mejores Vendedores

--- pages/mejoresvendedores.js
+++ pages/mejoresvendedores.js
@@ -1,56 +1,44 @@
 import React from 'react';
 import Layout from '../components/Layout';
-
-// Imports de Recharts
+import { gql, useQuery } from '@apollo/client';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-// Import de los datos que usa Recharts
-const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-];
+const MEJORES_VENDEDORES = gql`
+    query mejoresVendedores {
+        mejoresVendedores {
+            total
+            vendedor {
+                nombre
+                email
+            }
+        }
+    }
+`;
 
-const MejoresVendedoresq = () => {
+const MejoresVendedores = () => {
+
+    const { data, loading, error } = useQuery(MEJORES_VENDEDORES);
+    // console.log(data)
+    // console.log(loading)
+    // console.log(error)
+
+    if (loading) return 'Cargando...';
+
+    // console.log(data.mejoresVendedores);
+
+    const { mejoresVendedores } = data;
+
+    const vendedorGrafica = [];
+
+    mejoresVendedores.map((vendedor, index) => {
+        vendedorGrafica[index] = {
+            ...vendedor.vendedor[0],
+            total: vendedor.total
+        }
+    })
+    console.log(vendedorGrafica);
+
+
     return ( 
         <Layout>
             <h1 className='text-2xl text-gray-800 font-light'>Mejores Vendedores</h1>
@@ -60,7 +48,7 @@ const MejoresVendedoresq = () => {
                     className='mt-10'
                     width={600}
                     height={500}
-                    data={data}
+                    data={vendedorGrafica}
                     margin={{
                         top: 5,
                         right: 30,
@@ -69,11 +57,11 @@ const MejoresVendedoresq = () => {
                     }}
                 >
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
+                    <XAxis dataKey="nombre" />
                     <YAxis />
                     <Tooltip />
                     <Legend />
-                    <Bar dataKey="pv" fill="#3182CE" />
+                    <Bar dataKey="total" fill="#3182CE" />
                 </BarChart>
             </ResponsiveContainer>
 
@@ -81,4 +69,4 @@ const MejoresVendedoresq = () => {
      );
 }
  
-export default MejoresVendedoresq;
+export default MejoresVendedores;
