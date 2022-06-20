02ef3baae665ab605b065523975ccbbcddd7040a
Luis Saucedo <luigisaucedo.ls@gmail.com>
2022-06-20 05:53:35 -0400

    Sección Pedido: Creando el Query y Componente para mostrar los pedidos

--- pages/pedidos.js
+++ pages/pedidos.js
@@ -1,19 +1,59 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
 import Link from "next/link";
+import { gql, useQuery } from "@apollo/client";
+import Pedido from "../components/Pedido";
 
+const OBTENER_PEDIDOS = gql`
+  query ObtenerPedidos {
+    obtenerPedidos {
+      id
+      pedido {
+        id
+        cantidad
+      }
+      total
+      cliente
+      vendedor
+      fecha
+      estado
+    }
+  }
+`;
 
+const Pedidos = () => {
 
-const Pedidos = () => (
-  <div>
-    <Layout>
-        <h1 className="text-2xl text-gray-800 font-light">Pedidos</h1>
+  const { data, loading, error } = useQuery(OBTENER_PEDIDOS);
+  
+  if (loading) return 'Cargando...';
+  
+  const { obtenerPedidos } = data; // al profe le salió como variable: obtenerPedidosVendedor
+  console.log(data);
+  console.log(loading);
+  console.log(error);
 
-        <Link href="/nuevopedido">
-          <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Nuevo Pedido</a>
-        </Link>
-    </Layout>
-  </div>
-)
+  return (
+    <div>
+      <Layout>
+          <h1 className="text-2xl text-gray-800 font-light">Pedidos</h1>
+  
+          <Link href="/nuevopedido">
+            <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Nuevo Pedido</a>
+          </Link>
+
+          { obtenerPedidos.length === 0 ? (
+            <p className="mt-5 text-center text-2xl">No hay pedidos aún</p>
+          ) : (
+            obtenerPedidos.map( pedido => (
+              <Pedido
+                key={pedido.id}
+                pedido={pedido}
+              />
+            ))
+          )}
+      </Layout>
+    </div>
+  )
+}
 
 export default Pedidos;
