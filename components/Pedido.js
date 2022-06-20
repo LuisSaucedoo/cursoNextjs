a4da7a8d75536c6c0fff9a943024b86ebb49d689
Luis Saucedo <luigisaucedo.ls@gmail.com>
2022-06-20 08:16:24 -0400

    Sección Pedido: Eliminar Pedidos

--- components/Pedido.js
+++ components/Pedido.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { gql, useMutation } from '@apollo/client';
+import Swal from 'sweetalert2';
 
 const ACTUALIZAR_PEDIDO = gql`
     mutation actualizarPedido($id: ID!, $input: PedidoInput) {
@@ -9,12 +10,19 @@ const ACTUALIZAR_PEDIDO = gql`
     }
 `;
 
+const ELIMINAR_PEDIDO = gql`
+    mutation eliminarPedido($id: ID!) {
+        eliminarPedido(id: $id)
+    }
+`;
+
 const Pedido = ({pedido}) => {
 
     const { id, total, cliente:{ nombre, apellido, telefono, email }, estado, cliente } = pedido;
 
     // Mutation para cambiar el estado de un pedido
     const [ actualizarPedido ] = useMutation(ACTUALIZAR_PEDIDO);
+    const [ eliminarPedido ] = useMutation(ELIMINAR_PEDIDO);
 
     const [ estadoPedido, setEstadoPedido ] = useState(estado);
     const [ clase, setClase ] = useState('');
@@ -53,6 +61,38 @@ const Pedido = ({pedido}) => {
         } catch (error) {
             console.log(error);
         }
+    }
+
+    const confirmarEliminarPedido = () => {
+        Swal.fire({
+            title: '¿Deseas eliminar este pedido?',
+            text: "Esta acción no se puede deshacer !",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Eliminar',
+            cancelButtonText: 'No, Cancelar'
+          }).then( async (result) => {
+            if (result.isConfirmed) {
+                
+                // En esta parte se elimia el pedido con el mutation
+                try {
+                    await eliminarPedido({
+                        variables: {
+                            id: id
+                        }
+                    });
+                    Swal.fire(
+                        'Eliminado',
+                        data.eliminarPedido,
+                        'success'
+                    );
+                } catch(error) {
+                    console.log(error);
+                }
+            }
+          })
     }
 
     return ( 
@@ -106,6 +146,7 @@ const Pedido = ({pedido}) => {
 
                 <button
                     className='flex items-center mt-4 bg-red-800 px-5 py-2 inline-block text-white rounded leading-tight uppercase text-xs font-bold'
+                    onClick={ () => confirmarEliminarPedido() }
                 >
                     Eliminar Pedido
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
