e2241be48cab7d9cecb9b62c7d89971228a64338
Luis Saucedo <luigisaucedo.ls@gmail.com>
2022-06-20 00:12:24 -0400

    Sección Pedido: Creando el Componente de Total a pagar

--- pages/nuevopedido.js
+++ pages/nuevopedido.js
@@ -3,6 +3,7 @@ import Layout from '../components/Layout';
 import AsignarCliente from '../components/pedidos/AsignarCliente';
 import AsignarProductos from '../components/pedidos/AsignarProductos';
 import ResumenPedido from '../components/pedidos/ResumenPedido';
+import Total from '../components/pedidos/Total';
 
 // Context de pedidos
 import PedidoContext from '../context/pedidos/PedidoContext';
@@ -22,6 +23,12 @@ const NuevoPedido = () => {
                     <AsignarCliente/>
                     <AsignarProductos/>
                     <ResumenPedido/>
+                    <Total/>
+
+                    <button
+                        type='button'
+                        className={` bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900`}
+                    >Registrar Pedido</button>
                 </div>
             </div>
         </Layout>
