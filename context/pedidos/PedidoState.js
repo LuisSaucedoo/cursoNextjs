c4397917de6c93f7abcc0f204f5a29e2bd055ce3
Luis Saucedo <luigisaucedo.ls@gmail.com>
2022-06-20 03:14:38 -0400

    Sección Pedido: Ajustando la función que almacena el state de Prodcutos

--- context/pedidos/PedidoState.js
+++ context/pedidos/PedidoState.js
@@ -31,10 +31,22 @@ const PedidoState = ({children}) => {
     }
 
     // Modifica los productos
-    const agregarProducto = productos => {
+    const agregarProducto = productosSeleccionados => {
+
+        let nuevoState;
+        if (state.productos.length > 0) {
+            // Tomar del segundo arreglo una copia para asignarlo al primero
+            nuevoState = productosSeleccionados.map( producto => {
+                const nuevoObjeto = state.productos.find( productoState => productoState.id === producto.id );
+                return { ...producto, ...nuevoObjeto }
+            })
+        } else {
+            nuevoState = productosSeleccionados;
+        }
+
         dispatch({
             type: SELECCIONAR_PRODUCTO,
-            payload: productos
+            payload: nuevoState
         })
     }
 
