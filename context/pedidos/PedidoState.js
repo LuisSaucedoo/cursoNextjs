c81d71deb089d49750459e5ac167173d8aedb1c0
Luis Saucedo <luigisaucedo.ls@gmail.com>
2022-06-20 03:24:56 -0400

    Sección Pedido: Creando una función para el Total a pagar

--- context/pedidos/PedidoState.js
+++ context/pedidos/PedidoState.js
@@ -5,7 +5,8 @@ import PedidoReducer from './PedidoReducer';
 import {
     SELECCIONAR_CLIENTE,
     SELECCIONAR_PRODUCTO,
-    CANTIDAD_PRODUCTOS
+    CANTIDAD_PRODUCTOS,
+    ACTUALIZAR_TOTAL
 } from '../../types'
 
 const PedidoState = ({children}) => {
@@ -58,13 +59,21 @@ const PedidoState = ({children}) => {
         })
     }
 
+    const actualizarTotal = () => {
+        dispatch({
+            type: ACTUALIZAR_TOTAL
+        })
+    }
+
     return (
         <PedidoContext.Provider
             value={{
                 productos: state.productos,
+                total: state.total,
                 agregarCliente,
                 agregarProducto,
-                cantidadProductos
+                cantidadProductos,
+                actualizarTotal
             }}
         > {children}
         </PedidoContext.Provider>
